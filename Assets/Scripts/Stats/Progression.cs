e4d3c5fad0c8a02d9fb104052eff7eca776073be
David <da.halliday@gmail.com>
2021-05-23 09:07:11 +0100

    Character Progression - XP and Stat Lookup
    Add XP debug, Update XP to be saveable, add Stat Lookup table.

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Stats
@@ -7,22 +8,36 @@ namespace RPG.Stats
     {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
-            foreach(ProgressionCharacterClass progressionClass in characterClasses)
+            BuildLookup();
+
+            float[] levels = lookupTable[characterClass][stat];
+            if(levels.Length < level) { return 0; }
+
+            return levels[level - 1];
+        }
+
+        private void BuildLookup()
+        {
+            if(lookupTable != null) { return; }
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach (ProgressionCharacterClass progessionClass in characterClasses)
             {
-                if (progressionClass.characterClass != characterClass) { continue; }
+                var statLookupTable = new Dictionary<Stat, float[]>();
 
-                foreach(ProgressionStat progressionStat in progressionClass.stats)
+                foreach(ProgressionStat progressionStat in progessionClass.stats)
                 {
-                    if(progressionStat.stat != stat) { continue; }
-
-                    if(progressionStat.levels.Length < level) { continue; }
-
-                    return progressionStat.levels[level - 1];
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
                 }
+
+                lookupTable[progessionClass.characterClass] = statLookupTable;
             }
-            return 0;
         }
 
         [System.Serializable]
